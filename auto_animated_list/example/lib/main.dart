48a9e98cbaf336d761a8383b30bb9995960ca90e
Marc Sanny <marc.sanny@10clouds.com>
2022-07-05 21:53:31 +0200

    feat: Add example project

--- auto_animated_list/example/lib/main.dart
+++ auto_animated_list/example/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:auto_animated_list/auto_animated_list.dart';
+import 'package:example/utils/fruits.dart';
 import 'package:flutter/material.dart';
+import 'package:fuzzy/fuzzy.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,101 +10,100 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Auto Animated List Example',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  static const _limit = 10;
+  String _query = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<String> get _displayedFruits {
+    final fuzzy = Fuzzy<String>(
+      Fruits.list,
+      options: FuzzyOptions(
+        threshold: 0.5,
+        keys: [
+          WeightedKey(name: 'name', getter: (fruit) => fruit, weight: 1),
+        ],
+      ),
+    );
+    final results = fuzzy.search(_query);
+    return results
+        .getRange(0, results.length > _limit ? _limit : results.length)
+        .map((result) => result.item)
+        .toList(growable: false);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Auto Animated List Example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            _SearchInputField(
+              onChanged: (value) => setState(() => _query = value),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(height: 16),
+            Expanded(
+              child: AutoAnimatedList<String>(
+                items: _displayedFruits,
+                itemBuilder: (context, fruit, index, animation) {
+                  return SizeFadeTransition(
+                    animation: animation,
+                    child: ListTile(
+                      leading: Text('${index + 1}'),
+                      title: Text(fruit),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _SearchInputField extends StatelessWidget {
+  const _SearchInputField({
+    Key? key,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final ValueSetter<String> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onChanged: onChanged,
+      decoration: const InputDecoration(
+        labelText: 'Search',
+        focusedBorder: OutlineInputBorder(),
+        enabledBorder: OutlineInputBorder(),
+      ),
     );
   }
 }

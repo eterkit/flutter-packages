30ade2a6f046530bdb0a25f17251670e00ffc94f
Marc Sanny <marc.sanny@htdevelopers.com>
2023-09-14 10:53:35 +0200

    chore: Remove wip calendly package

--- calendly/lib/src/extensions/bool_to_number.dart
+++
@@ -1,5 +0,0 @@
-/// Helper extension to convert `bool` to `int`.
-extension BoolToNumberExtension on bool {
-  /// Integer representation of `this` `bool`.
-  int get number => this == true ? 1 : 0;
-}
